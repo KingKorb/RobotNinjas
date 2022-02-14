d59aa3bd492a1a172c64aabf3cb5173c6f9f7e70
linzhanguca <lzhang12@uca.edu>
2022-02-13 19:51:49 -0600

    update and validate urdf display in rviz

--- ninjasrobot_simulation/launch/sim_ninjasrobot.launch.py
+++ ninjasrobot_simulation/launch/sim_ninjasrobot.launch.py
@@ -1,62 +1,114 @@
-import launch
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
-import os
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='ninja_description').find('ninja_description')
-    default_model_path = os.path.join(pkg_share, 'src/ninja_description.urdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    pkg_share_path = get_package_share_path("ninjasrobot_simulation")
+    default_urdf_path = pkg_share_path / "urdf/ninjasrobot.urdf"
+    default_rviz_config_path = pkg_share_path / "rviz/ninjasrobot.rviz"
 
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    gui_arg = DeclareLaunchArgument(
+        name="gui",
+        default_value="true",
+        choices=["true", "false"],
+        description="Flag to enable joint_state_publisher_gui",
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    urdf_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=str(default_urdf_path),
+        description="Absolute path to robot urdf file",
     )
-    
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    rviz_arg = DeclareLaunchArgument(
+        name="rvizconfig",
+        default_value=str(default_rviz_config_path),
+        description="Absolute path to rviz config file",
     )
-
-    spawn_entity = launch_ros.actions.Node(
-    package='gazebo_ros',
-    executable='spawn_entity.py',
-    arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
-    output='screen'
+    sim_time_arg = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="true",
+        choices=["true", "false"],
+        description="Flag to enable use_sim_time",
     )
 
-    robot_localization_node = launch_ros.actions.Node(
-       package='robot_localization',
-       executable='ekf_node',
-       name='ekf_filter_node',
-       output='screen',
-       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), value_type=str
     )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
+                "robot_description": robot_description,
+            }
+        ],
+    )
 
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),    
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        spawn_entity,
-        robot_localization_node,
-        rviz_node
-    ])
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        condition=UnlessCondition(LaunchConfiguration("gui")),
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        condition=IfCondition(LaunchConfiguration("gui")),
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
+    )
+
+    # execute_gazebo = ExecuteProcess(
+    #     cmd=[
+    #         "gazebo",
+    #         "--verbose",
+    #         "-s",
+    #         "libgazebo_ros_init.so",
+    #         "-s",
+    #         "libgazebo_ros_factory.so",
+    #     ],
+    #     output="screen",
+    # )
+
+    # spawn_entity = launch_ros.actions.Node(
+    # package='gazebo_ros',
+    # executable='spawn_entity.py',
+    # arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
+    # output='screen'
+    # )
+
+    # robot_localization_node = launch_ros.actions.Node(
+    #    package='robot_localization',
+    #    executable='ekf_node',
+    #    name='ekf_filter_node',
+    #    output='screen',
+    #    parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    # )
+
+    return LaunchDescription(
+        [
+            gui_arg,
+            urdf_arg,
+            rviz_arg,
+            sim_time_arg,
+            joint_state_publisher_node,
+            joint_state_publisher_gui_node,
+            robot_state_publisher_node,
+            # spawn_entity,
+            # robot_localization_node,
+            rviz_node,
+        ]
+    )
--- ninjasrobot_simulation/setup.py
+++ ninjasrobot_simulation/setup.py
@@ -1,25 +1,34 @@
+import os
+from glob import glob
 from setuptools import setup
 
-package_name = 'ninjasrobot_simulation'
+package_name = "ninjasrobot_simulation"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.launch.py")),
+        ),
+        (os.path.join("share", package_name, "urdf"), glob(os.path.join("urdf", "*"))),
+        (
+            os.path.join("share", package_name, "rviz"),
+            glob(os.path.join("rviz", "*.rviz")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='palebluedotian',
-    maintainer_email='lzhang12@uca.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="palebluedotian",
+    maintainer_email="lzhang12@uca.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
