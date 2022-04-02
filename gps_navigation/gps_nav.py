7abbf4fdee4809eb3b23f2d17b429b82d5d2c23b
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2022-04-02 17:23:43 -0500

    Update gps_nav.py

--- gps_navigation/gps_nav.py
+++ gps_navigation/gps_nav.py
@@ -4,6 +4,8 @@ import numpy as np
 import serial
 import time
 
+
+# setup PWM pins
 Motor1PWM = 12
 Motor1D = 24
 Motor1E= 22
@@ -17,7 +19,6 @@ GPIO.setup(Motor1E, GPIO.OUT)
 GPIO.setup(Motor2PWM, GPIO.OUT)
 GPIO.setup(Motor2D, GPIO.OUT)
 GPIO.setup(Motor2E, GPIO.OUT)
-
 pwm = GPIO.PWM(Motor1PWM, 1000)
 pwm2 = GPIO.PWM(Motor2PWM, 1000)
 pwm.start(0)
@@ -78,49 +79,50 @@ def stop():
     GPIO.output(Motor1E,GPIO.LOW)
     GPIO.output(Motor2E,GPIO.LOW)
 
-        
+# setup serial communication       
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 ser.reset_input_buffer()
 target_lat = [35.08331,35.08335,35.08331,35.08343,35.08343,3508343,35.08341]
 target_lon = [-92.45882,-92.45871,-92.45862,-92.45859,-92.45867,-92.45879,-92.45881]
 i=0
 
-while True:
-#for _ in range(2000): 
-    if ser.in_waiting > 0:
-        ser.write(b"Hello from Raspberry Pi!\n")
-        line = ser.readline().decode('utf-8').rstrip().split(',')
-        Lat = float(line[0])
-        Long = float(line[1])
-        Ori = float(line[2])
-        
-        #x,y,z = pm.geodetic2enu(35.08341, -92.45879 , 95.0976, Lat, Long, 95.0976)
-        x,y,z = pm.geodetic2enu(target_lat[i], target_lon[i] , 95.0976, Lat, Long, 95.0976)
-        
-        theta = np.deg2rad(Ori)
-        if theta > np.pi:
-            theta = theta - 2*np.pi
-        elif theta > 2*np.pi:
-            theta = theta - 2*np.pi
-        phe = np.arctan2(y, x)
-        del_theta = phe - theta
-        
-        if del_theta > np.pi/6:
-            left()
-        elif del_theta < -np.pi/6:
-            right()
-        else:
-            forward()
-        
-        if np.sqrt(x**2+y**2) < 2:  # TODO: tweak this number to get a reasonable target circle
-             i += 1
-#         print(x,y,z)
-#         print(del_theta)
-        print(f'distance: {np.linalg.norm((x,y))}')
-        print(f'GPS Coords: {Lat, Long, Ori}')
-        print(f'current target: {i}')
-        print('\n')
-
-        time.sleep(.05)
-        
-GPIO.cleanup()
+try:
+    while True:
+        if ser.in_waiting > 0:
+            # read gps coords and facing angle (relative to east)
+            line = ser.readline().decode('utf-8').rstrip().split(',')
+            Lat = float(line[0])
+            Long = float(line[1])
+            Ori = float(line[2])
+            # convert target coords in Geodetic frame to ENU frame. Robot's location is the origin   
+            x,y,z = pm.geodetic2enu(target_lat[i], target_lon[i] , 95.0976, Lat, Long, 95.0976)
+            # compute pointing angle adjustment
+            theta = np.deg2rad(Ori)  # robot facing angle
+            if theta > np.pi:  # restric theta in range: (-pi, pi)
+                theta = theta - 2*np.pi
+            elif theta > 2*np.pi:
+                theta = theta - 2*np.pi
+            phe = np.arctan2(y, x)  # angle of robot-target line vs. East
+            del_theta = phe - theta  # robot facing angle adjustment
+            if del_theta > np.pi/6:  # change robot facing angle if it is off too much
+                left()
+            elif del_theta < -np.pi/6:
+                right()
+            else:
+                forward()  # otherwise, go straightfoward
+            # detect if robot within the range of target
+            if np.sqrt(x**2+y**2) < 2:  # tweak this number so that robot find next target earlier or later
+                 i += 1
+            # debugging prints
+            print('---')
+            print(f'current target: {i+1}')
+            print(f'distance to target: {np.linalg.norm((x,y))}')
+            print(f'robot pose (Geodetic): {Lat, Long, Ori}')
+            print(f'target location: {x, y}')
+            print(f'angle offset: {del_theta}')
+            
+            time.sleep(.05)
+            
+except KeyboardInterrupt:  # ctrl-c to stop robot
+    GPIO.cleanup()
+    
